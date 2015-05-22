af61193c8feea5df2e0471397f432cc50571b815
Ruslan <syslao@gmail.com>
2015-05-22 15:13:16 +0300

    js add up botton

--- app/js/main.js
+++ app/js/main.js
@@ -50,4 +50,18 @@ var gallery = (function() {
 
 $(document).ready(function(){
     gallery.init();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.up').fadeIn();
+        } else {
+            $('.up').fadeOut();
+        }
+    });
+    $('.up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
